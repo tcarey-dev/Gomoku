26224d7d10736e91d626442cf0eb60795b28aa2d
Thomas <38011284+tcarey-dev@users.noreply.github.com>
2023-07-31 17:42:24 -0500

    Removed redundant fields

--- gomoku/src/main/java/learn/gomoku/players/SmartNPC.java
+++ gomoku/src/main/java/learn/gomoku/players/SmartNPC.java
@@ -8,19 +8,6 @@ import java.util.Random;
 
 public class SmartNPC extends RandomPlayer {
 
-    private static String[] titles = {"Dr.", "Professor", "Chief Exec", "Specialist", "The Honorable",
-            "Prince", "Princess", "The Venerable", "The Eminent"};
-    private static String[] names = {
-            "Evelyn", "Wyatan", "Jud", "Danella", "Sarah", "Johnna",
-            "Vicki", "Alano", "Trever", "Delphine", "Sigismundo",
-            "Shermie", "Filide", "Daniella", "Annmarie", "Bartram",
-            "Pennie", "Rafael", "Celine", "Kacey", "Saree", "Tu",
-            "Erny", "Evonne", "Charita", "Anny", "Mavra", "Fredek",
-            "Silvio", "Cam", "Hulda", "Nanice", "Iolanthe", "Brucie",
-            "Kara", "Paco"};
-    private static String[] lastNames = {"Itch", "Potato", "Mushroom", "Grape", "Mouse", "Feet",
-            "Nerves", "Sweat", "Sweet", "Bug", "Piles", "Trumpet", "Shark", "Grouper", "Flutes", "Showers",
-            "Humbug", "Cauliflower", "Shoes", "Hopeless", "Zombie", "Monster", "Fuzzy"};
     private final String[][] board = new String[Gomoku.WIDTH][Gomoku.WIDTH];
     private final Random random = new Random();
 
